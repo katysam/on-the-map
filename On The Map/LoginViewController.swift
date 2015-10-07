006acb24557bdaef0d1e337d215720f8fea7e22e
katysam <kathrynsamalin@gmail.com>
2015-10-07 08:31:28 -0400

    Removed FB frameworks and FBLoginButton.

--- On The Map/LoginViewController.swift
+++ On The Map/LoginViewController.swift
@@ -41,7 +41,7 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        FBSDKLoginButton()
+//        FBSDKLoginButton()
         
         /* Get the app delegate */
         appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
